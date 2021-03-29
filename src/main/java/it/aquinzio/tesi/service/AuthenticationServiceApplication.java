752d2377b1d4d01ea154499f14c4a49200d04f62
quan83 <aquinzio08@gmail.com>
2021-03-29 20:07:35 +0200

    refactoring code - add lombok dependencies

--- src/main/java/it/aquinzio/tesi/service/AuthenticationServiceApplication.java
+++ src/main/java/it/aquinzio/tesi/service/AuthenticationServiceApplication.java
@@ -1,13 +1,13 @@
-package it.aquinzio.tesi.service.inventory;
+package it.aquinzio.tesi.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
 
 }
